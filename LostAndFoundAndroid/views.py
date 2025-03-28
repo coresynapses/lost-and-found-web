1ef95d11b96dcc774a7b4e3d8b944f3d5764e337
Charlie Suarez <charlie.suarez273@protonmail.com>
2025-03-28 09:59:16 -0500

    Updated to serve example data

--- LostAndFoundAndroid/views.py
+++ LostAndFoundAndroid/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from lostfound.models import *
 
 def android(request):
-    return HttpResponse("Android.")
+    data = {
+        "items": [
+            {
+                "name": "shirt",
+                "description": "just a shirt",
+                "date": "1/1/2020",
+                "category": "apparel",
+            },
+            {
+                "name": "credit card",
+                "description": "a black card",
+                "date": "2/2/2020",
+                "category": "finance",
+            },
+        ]
+    }
+    
+    for item in Item.objects.all():
+        data["items"].append({
+            "name": f"{item.itemName}",
+            "description": f"{item.description}",
+            "date": f"{item.dateReported}",
+            "category": f"{item.category}"
+        })
+        print(f"{item}")
+
+    return JsonResponse(data)
