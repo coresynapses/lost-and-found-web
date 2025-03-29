09fdb2f32a02bd88de3869466712ed0f1679ecbf
Zetta-Hz <hinojosa.a.dev@gmail.com>
2025-03-29 14:45:42 -0500

    Set up routes for visuals

--- TamiuLostAndFound/settings.py
+++ TamiuLostAndFound/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "TamiuLostAndFound/static"]  #  app specific
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
