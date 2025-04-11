4edf772f37a1c73c3fddafcc5609bdcf895645ca
Andres <andreslopezdelagarza@gmail.com>
2025-04-10 23:59:20 -0500

    Included homepage as the index link, modified index.html login and register redirect buttons

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import home, login_page, register_page, logout_user
 
 urlpatterns = [
-    path('home/', home, name="home"),
-    path('', login_page, name="login"),
+    path('', home, name="home"),
+    path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
     path("logout/", logout_user, name="logout"),
 ]
